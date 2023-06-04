668b9dfa4dc6ebcb81c379d24d315efd8afa983b
ricardocamargo0 <rncamargo58@gmail.com>
2023-06-04 18:33:37 -0300

    script modal, css and index

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,24 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+const modal = document.getElementById("modal");
+const modalOpenButtons = document.querySelectorAll(".modal__button-open");
+
+function openModal() {
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+modalOpenButtons.forEach((button) => {
+  button.addEventListener("click", openModal);
+});
+
+const modalCloseButton = document.getElementById("close");
+
+modalCloseButton.addEventListener("click", closeModal);
+window.addEventListener("click", (event) => {
+  if (event.target === modal || event.target === modalCloseButton) {
+    closeModal();
+  }
+});
